6583b19a864a5c1f041283d2cb0a43c46b1e632f
jankapunkt <jkuester@uni-bremen.de>
2018-05-09 21:11:15 +0200

    1.0.1

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'cquencial:bpmn-engine',
-  version: '0.1.0',
+  version: '0.1.1',
   // Brief, one-line summary of the package.
   summary: 'Base package for including paed01/bpmn-engine.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/cquencial/meteor-bpm-engine.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md',
